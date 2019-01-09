1f47f14fdc27ddb28a2bdffabb812e8bf47b97cb
iyon1995 <33445525+iyon1995@users.noreply.github.com>
2019-01-09 22:15:14 +0100

    9.1
    settle account

--- TheSpy/Controller/SettleAccountsController.php
+++ TheSpy/Controller/SettleAccountsController.php
@@ -10,6 +10,7 @@
 require_once '../tools/CookieTools.php';
 require_once '../Service/UserService.class.php';
 require_once '../tools/PhpValidate.php';
+require_once '../tools/CookieTools.php';
 
 if(!empty($_POST['isSpy'])){
     $isSpy = $_POST['isSpy'];
@@ -18,6 +19,8 @@ if(!empty($_POST['isSpy'])){
 if(!empty($_POST['isWin'])){
     $isWin = $_POST['isWin'];
 }
+$userId = getCookieVal("userId");
+
 $result = array("isWin" => 0,"isSpyWin" => 0);
 if ($isWin != "L"){
     $result["isWin"] = 1;
@@ -34,6 +37,8 @@ $exp = getCookieVal("exp");
 
 $userService = new UserService();
 $userService -> settleAccounts($userId,$exp,$result);
+
+echo "E";
 
 
 ?>
